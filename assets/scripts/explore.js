74f0449d28e4d343a3fad729a51cf8e47bc5d10a
czhang0003 <ciz003@ucsd.edu>
2026-05-05 20:53:30 -0700

    finished speech synthesis

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,48 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+
+  const textArea = document.querySelector('#text-to-speak');
+  const voiceSelect = document.querySelector('#voice-select');
+  const button = document.querySelector('button');
+  const face = document.querySelector('#explore img');
+
+  speechSynthesis.getVoices();
+
+  let voices = [];
+
+  //loading voices into dropdown
+  function loadVoices() {
+    voices = speechSynthesis.getVoices();
+
+    voiceSelect.innerHTML = '<option disabled selected>Select Voice:</option>';
+
+    voices.forEach((voice, index) => {
+      const option = document.createElement('option');
+      option.value = index;
+      option.textContent = `${voice.name} (${voice.lang})`;
+      voiceSelect.appendChild(option);
+    });
+  }
+
+  speechSynthesis.onvoiceschanged = loadVoices;
+
+  //play voice on click and changes images
+  button.addEventListener('click', () => {
+    const text = textArea.value;
+    const utterance = new SpeechSynthesisUtterance(text);
+
+    const selectedIndex = voiceSelect.value;
+    utterance.voice = voices[selectedIndex];
+
+    utterance.onstart = () => {
+      face.src = 'assets/images/smiling-open.png';
+    };
+
+    utterance.onend = () => {
+      face.src = 'assets/images/smiling.png';
+    };
+
+    speechSynthesis.speak(utterance);
+  });
 }
